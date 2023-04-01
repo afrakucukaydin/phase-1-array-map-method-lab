5daf86d48cedfc877fff8c385a120e4c730cf907
Afra Kucukaydin <afrakucukaydin@gmail.com>
2023-04-01 22:46:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    let words = tutorial.split(' ');
+    words = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    return words.join(' ');
+  });
 }
+
+console.log(titleCased());
